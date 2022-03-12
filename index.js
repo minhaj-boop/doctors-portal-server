cf8150c52ebc01047686ca698a5809488d67f675
minhaj-boop <imminhajj@gmail.com>
2022-03-13 02:16:40 +0600

    connection established

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ app.use(cors());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f7vlo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect();
+        console.log("db connected");
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.error);
 
 
 app.get('/', (req, res) => {
