5991f5bab711848ae08cbfc3391e99bbf8671853
minhaj-boop <imminhajj@gmail.com>
2022-03-14 23:44:41 +0600

    get and post api created for appointments

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-
+app.use(express.json());
 //db connect
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f7vlo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -15,7 +15,27 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log("db connected");
+        const database = client.db('doctors-portal');
+        const appointmentsCollection = database.collection('appointments');
+
+        //get appointments
+        app.get('/appointment', async (req, res) => {
+            const email = req.query.email;
+            // const date = new Date(req.query.date).toLocaleDateString();
+            const date = req.query.date;
+            const query = { email: email, date: date };
+            const cursor = appointmentsCollection.find(query);
+            const appointments = await cursor.toArray();
+            res.json(appointments);
+        })
+
+        //post appointment
+        app.post('/appointment', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentsCollection.insertOne(appointment);
+            console.log(result);
+            res.json(result);
+        })
     }
     finally {
         // await client.close();
@@ -26,7 +46,7 @@ run().catch(console.error);
 
 
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.send('hello doctors portal')
 });
 
 app.listen(port, () => {
