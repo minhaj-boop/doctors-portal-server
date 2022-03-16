915eaa110b9e5eec9e30c6e806a593f5e13b1fee
minhaj-boop <imminhajj@gmail.com>
2022-03-17 01:39:26 +0600

    admin panel added along wtih nested routing

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ async function run() {
         await client.connect();
         const database = client.db('doctors-portal');
         const appointmentsCollection = database.collection('appointments');
+        const usersCollection = database.collection('users');
 
         //get appointments
         app.get('/appointment', async (req, res) => {
@@ -27,13 +28,51 @@ async function run() {
             const cursor = appointmentsCollection.find(query);
             const appointments = await cursor.toArray();
             res.json(appointments);
+        });
+
+        // // get admin
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
         })
 
         //post appointment
         app.post('/appointment', async (req, res) => {
             const appointment = req.body;
             const result = await appointmentsCollection.insertOne(appointment);
-            console.log(result);
+            res.json(result);
+        });
+
+        //post users
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = usersCollection.insertOne(user);
+            res.json(result);
+        });
+
+        //update google signin
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        //make admin
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            console.log('put', user);
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = usersCollection.updateOne(filter, updateDoc);
             res.json(result);
         })
     }
